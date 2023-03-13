76e0c325af02d2a067dc268c24ee506bf20d1f55
Navial <victor.denis@gmail.com>
2023-03-13 21:36:42 +0100

    feat: start BFS algorithm

--- src/Graph.java
+++ src/Graph.java
@@ -2,28 +2,65 @@ package src;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.rmi.server.SocketSecurityException;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.Map.Entry;
 
 public class Graph {
 
-    private Map< Integer, Set<Troncon>> map;
+    private Map<String, Set<Troncon>> mapTronconsDepart;
+
     // ajouter une file et un autre set qui servira de stocker toutes les stations
     //deja pacrourues pour BFS
 
     public Graph(File fileLignes, File fileTrancons) throws FileNotFoundException {
 
-
+        this.mapTronconsDepart = new HashMap<>();
         Scanner scLigne = new Scanner(fileLignes);
-        Scanner scTrancons = new Scanner(fileTrancons);
+        Scanner scTroncons = new Scanner(fileTrancons);
 
-        while (scLigne.hasNextLine()){
-            String value = scLigne.nextLine();
+        while (scTroncons.hasNextLine()){
+            String value = scTroncons.nextLine();
             String[] values = value.split(",");
-            Ligne ligne = new Ligne(Integer.parseInt(values[0]),Integer.parseInt(values[1]),values[2],values[3],values[4],Integer.parseInt(values[5]));
-
+            Troncon troncon = new Troncon(Integer.parseInt(values[0]),values[1],values[2],Integer.parseInt(values[3]));
+            if (!mapTronconsDepart.containsKey(troncon.getDepart())){
+                mapTronconsDepart.put(troncon.getDepart(), new HashSet<>());
+                mapTronconsDepart.get(troncon.getDepart()).add(troncon);
+            } else {
+                mapTronconsDepart.get(troncon.getDepart()).add(troncon);
+            }
         }
+    }
 
+    public void calculerCheminMinimisantNombreTroncons(String depart, String arrivee) {
+        Map<String, Troncon> mapBFS = new HashMap<>();
+        Deque<String> fileDesStationsAParcourir = new ArrayDeque<>();
+        Set<String> stationsParcourues = new HashSet<>();
+
+        mapBFS.put(depart, null);
+        fileDesStationsAParcourir.add(depart);
+
+        while(!fileDesStationsAParcourir.isEmpty()){
+            for (Troncon troncon : mapTronconsDepart.get(fileDesStationsAParcourir.poll())) {
+                mapBFS.put(troncon.getArrivee(), troncon);
+                stationsParcourues.add(troncon.getDepart());
+                if(troncon.getArrivee().equals(arrivee)){
+                    
+                }
+                fileDesStationsAParcourir.add(troncon.getArrivee());
+            }   
+
+        } 
+
+
+    }
+
+    public void calculerCheminMinimisantTempsTransport(String string, String string2) {
     }
 }
