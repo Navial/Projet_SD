a5b201a1807aa2c5121921776b88efa8f41856ac
Navial <victor.denis@gmail.com>
2023-03-13 21:55:01 +0100

    Fix: faute de frappe

--- src/Graph.java
+++ src/Graph.java
@@ -2,7 +2,6 @@ package src;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.rmi.server.SocketSecurityException;
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.HashMap;
@@ -10,32 +9,47 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
-import java.util.Map.Entry;
 
 public class Graph {
 
     private Map<String, Set<Troncon>> mapTronconsDepart;
+    private Map<Integer, Ligne> mapLigne;
 
     // ajouter une file et un autre set qui servira de stocker toutes les stations
     //deja pacrourues pour BFS
-
     public Graph(File fileLignes, File fileTrancons) throws FileNotFoundException {
-
         this.mapTronconsDepart = new HashMap<>();
-        Scanner scLigne = new Scanner(fileLignes);
-        Scanner scTroncons = new Scanner(fileTrancons);
+        // TODO add dans le file
+        this.mapLigne = new HashMap<>();
+        // TODO add dans le file
+        int numeroLigne = 1;
 
-        while (scTroncons.hasNextLine()){
-            String value = scTroncons.nextLine();
+        Scanner scLigne = new Scanner(fileLignes);
+        Scanner scTrancons = new Scanner(fileTrancons);
+
+        // TODO add dans le file
+        while (scLigne.hasNextLine()){
+            String value = scLigne.nextLine();
             String[] values = value.split(",");
-            Troncon troncon = new Troncon(Integer.parseInt(values[0]),values[1],values[2],Integer.parseInt(values[3]));
-            if (!mapTronconsDepart.containsKey(troncon.getDepart())){
-                mapTronconsDepart.put(troncon.getDepart(), new HashSet<>());
-                mapTronconsDepart.get(troncon.getDepart()).add(troncon);
+            Ligne ligne = new Ligne(Integer.parseInt(values[1]),values[2],values[3],values[4],Integer.parseInt(values[5]));
+            mapLigne.put(numeroLigne++,ligne);
+        }
+
+        while (scTrancons.hasNextLine()){
+            String value = scTrancons.nextLine();
+            String[] values = value.split(",");
+            Ligne ligne = mapLigne.get(Integer.parseInt(values[0]));
+            Troncon trancon = new Troncon(Integer.parseInt(values[0]),values[1],values[2],Integer.parseInt(values[3]),ligne );
+            System.out.println(trancon);
+            if (!mapTronconsDepart.containsKey(trancon.getDepart())){
+                mapTronconsDepart.put(trancon.getDepart(), new HashSet<>());
+                mapTronconsDepart.get(trancon.getDepart()).add(trancon);
             } else {
-                mapTronconsDepart.get(troncon.getDepart()).add(troncon);
+                mapTronconsDepart.get(trancon.getDepart()).add(trancon);
             }
         }
+
+
     }
 
     public void calculerCheminMinimisantNombreTroncons(String depart, String arrivee) {
